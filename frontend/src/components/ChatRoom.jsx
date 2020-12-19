9e77b85a62dc113d7b85a10ad4998efd2b700322
AmEr-Tinsley <amerhosni07@gmail.com>
2020-12-19 10:35:17 +0100

    ScrollToBottom

--- frontend/src/components/ChatRoom.jsx
+++ frontend/src/components/ChatRoom.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import axios from 'axios'
 function ChatRoom() {
     const [messages, setMessages] = useState([]);
@@ -9,25 +9,57 @@ function ChatRoom() {
                 setMessages([...messages, { content: response.data, isBot: true }]);
             })
         }
+
     }, [messages])
     const onEnterPress = (e) => {
 
         if (e.keyCode == 13 && e.shiftKey == false) {
             setMessages([...messages, { content: currentMessage, isBot: false }]);
-
             setCurrentMessage('')
             e.preventDefault();
 
         }
     }
-    return (
-        <div className="container-fluid h-100">
-            <div className="row justify-content-center h-100">
 
-                <div className="col-md-8 col-xl-6 chat">
-                    <div className="card">
-                        <div className="card-header msg_head">
-                            <div className="d-flex bd-highlight">
+    const AlwaysScrollToBottom = () => {
+        const elementRef = useRef();
+        useEffect(() => elementRef.current.scrollIntoView());
+        return <div ref={elementRef} />;
+    };
+    const Messages = () => (
+        <div className="card-body msg_card_body">
+            {
+                messages.map(message => {
+                    return (
+                        message.isBot ? <div className="d-flex justify-content-start mb-4">
+                            <div className="img_cont_msg">
+                                <img src="./_D.jpg" className="rounded-circle user_img_msg" />
+                            </div>
+                            <div className="msg_cotainer">
+                                {message.content}
+                            </div>
+                        </div> : <div className="d-flex justify-content-end mb-4">
+                                <div className="msg_cotainer_send">
+                                    {message.content}
+                                </div>
+                                <div className="img_cont_msg">
+                                    <img src="" />
+                                </div>
+                            </div>
+                    )
+                })
+            }
+            <AlwaysScrollToBottom />
+        </div>
+    )
+    return (
+        <div className="container-fluid h-100" >
+            <div className="row justify-content-center h-100" >
+
+                <div className="col-md-8 col-xl-6 chat" >
+                    <div className="card" >
+                        <div className="card-header msg_head" >
+                            <div className="d-flex bd-highlight" >
                                 <div className="img_cont">
                                     <img src="./_D.jpg" className="rounded-circle user_img" />
                                     <span className="online_icon"></span>
@@ -40,32 +72,7 @@ function ChatRoom() {
                             </div>
 
                         </div>
-                        <div className="card-body msg_card_body">
-                            {
-                                messages.map(message => {
-                                    return (
-                                        message.isBot ? <div className="d-flex justify-content-start mb-4">
-                                            <div className="img_cont_msg">
-                                                <img src="./_D.jpg" className="rounded-circle user_img_msg" />
-                                            </div>
-                                            <div className="msg_cotainer">
-                                                {message.content}
-                                            </div>
-                                        </div> : <div className="d-flex justify-content-end mb-4">
-                                                <div className="msg_cotainer_send">
-                                                    {message.content}
-                                                </div>
-                                                <div className="img_cont_msg">
-                                                    <img src="" />
-                                                </div>
-                                            </div>
-                                    )
-                                })
-                            }
-
-
-
-                        </div>
+                        <Messages />
                         <div className="card-footer">
                             <div className="input-group">
 
